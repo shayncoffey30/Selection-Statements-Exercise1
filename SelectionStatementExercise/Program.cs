814685e548dea9a071aad5c6deed1755d2678817
Shayn Coffey <Shayncoffey30@gmail.com>
2024-06-24 16:38:36 -0400

    excercise part 1 completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatements
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guessing Game
+            var r = new Random();
+            //var favNumber = r.Next(0, 1001);
+            var favNumber = 5;
+
+            Console.WriteLine("Pick a number from 1 - 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is to low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is to high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
         }
     }
 }
